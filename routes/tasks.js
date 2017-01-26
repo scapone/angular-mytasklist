5aa9fa4cec15824d06d1762e8dd959dd6b8e2723
Serhiy Haponenko <Serhiy_Haponenko@epam.com>
2017-01-26 12:24:15 +0200

    first commit

---
+++ routes/tasks.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://brad:brad@ds153745.mlab.com:53745/mytasklist_sg', ['tasks']);
+
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err, tasks){
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(tasks);
+    });
+});
+
+router.get('/tasks/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(task);
+    });
+});
+
+router.post('/task', function(req, res, next){
+    var task = req.body;
+
+    if(!task.title || (task.isDone + '')) {
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.save(task, function(err, task) {
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(task);
+        });
+    }
+});
+
+router.delete('/tasks/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(task);
+    });
+});
+
+router.put('/tasks/:id', function(req, res, next){
+    var task = req.body;
+    var updTask = {};
+
+    if (task.isDone) {
+        updTask.isDone = task.isDone;
+    }
+
+    if (task.title) {
+        updTask.title = task.title;
+    }
+
+    if (!updTask) {
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    } else {
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, updTask, {}, function(err, task){
+            if (err) {
+                res.send(err);
+            }
+
+            res.json(updTask);
+    });
+    }
+});
+
+module.exports = router;
